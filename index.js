9a80119d0f4dda720272fca048ac94f377b854e4
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-07-30 00:46:29 +0530

    moving code to typescript

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
-const menu = [
-  { name: "Margherita", price: 8 },
-  { name: "Pepperoni", price: 10 },
-  { name: "Hawaiian", price: 10 },
-  { name: "Veggie", price: 8 },
+var menu = [
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 8 },
 ];
-
-let cashInRegister = 100;
-const orderQueue = [];
-let nextOrderId = 1;
-
+var cashInRegister = 100;
+var orderQueue = [];
+var nextOrderId = 1;
 // FUNCTION TO ADD NEW PIZZA
 function addNewPizza(pizzaObj) {
-  menu.push(pizzaObj);
+    menu.push(pizzaObj);
 }
-
-
 // FUNCTION TO PLACE ORDER
 function placeOrder(pizzaName) {
-  const selectedPizza = menu.find((item) => item.name === pizzaName);
-  cashInRegister += selectedPizza.price;
-  const newOrder = {
-    id: nextOrderId++,
-    pizza: selectedPizza,
-    status: "ordered",
-  };
-  orderQueue.push(newOrder);
-  return newOrder;
+    var selectedPizza = menu.find(function (item) { return item.name === pizzaName; });
+    cashInRegister += selectedPizza.price;
+    var newOrder = {
+        id: nextOrderId++,
+        pizza: selectedPizza,
+        status: "ordered",
+    };
+    orderQueue.push(newOrder);
+    return newOrder;
 }
-
-
 // FUNCTION TO MARK THE ORDER COMPLETE
 function completeOrder(orderId) {
-  const order = orderQueue.find((item) => item.id === orderId);
-  return order;
+    var order = orderQueue.find(function (item) { return item.id === orderId; });
+    order.status = "completed"
+    return order;
 }
-
-
-addNewPizza({name: "Chizza", price: 7})
-console.log(menu)
+addNewPizza({ name: "Chizza", price: 7 });
+console.log(menu);
 console.log(placeOrder("Margherita"));
 console.log(placeOrder("Pepperoni"));
-console.log(orderQueue)
-console.log(completeOrder(1))
-console.log(cashInRegister)
+console.log(orderQueue);
+console.log(completeOrder(1));
+console.log(cashInRegister);
